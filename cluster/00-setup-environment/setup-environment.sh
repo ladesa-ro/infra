25af4efe7e73d6ca60cd0f48368b583529953584
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-17 21:29:28 -0400

    fix(cluster/00-setup-environment/script): sha256 spacing

--- cluster/00-setup-environment/setup-environment.sh
+++ cluster/00-setup-environment/setup-environment.sh
@@ -28,7 +28,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256";
 
 # verificação do hash com o binário baixado
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check;
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 # instalação do kubectl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl;
