1dd8cb0ec235e45f52908e8cc4d4bb7785d9e150
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-18 14:43:51 -0400

    feat(registry): postgres database deployment

--- services/registry/deploy.sh
+++ services/registry/deploy.sh
@@ -5,4 +5,7 @@ set -xe
 kubectl apply -f ./00-volume-config.yaml \
                   ./00-volume-log.yaml \
                   ./00-volume-storage.yaml \
-                  ./01-deployment.yaml ;
+                  ./01-postgres-volume-storage.yaml \
+                  ./02-postgres-deployment.yaml \
+                  ./02-redis-deployment.yaml \
+                  ./03-deployment.yaml;
