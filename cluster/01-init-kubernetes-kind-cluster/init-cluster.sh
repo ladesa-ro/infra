470953cfbc701dd2a04071664eb32ebbc991b071
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-18 15:53:57 -0400

    fix(registry): secrets and pv/pvc names

--- cluster/01-init-kubernetes-kind-cluster/init-cluster.sh
+++ cluster/01-init-kubernetes-kind-cluster/init-cluster.sh
@@ -5,3 +5,5 @@ set -xe
 kind delete cluster;
 
 kind create cluster --config=./init-cluster-manifest.yml;
+
+(kubectl taint nodes --all node-role.kubernetes.io/control-plane- || exit 0);
--- services/registry/deploy.sh
+++ services/registry/deploy.sh
@@ -3,9 +3,10 @@
 set -xe
 
 kubectl apply -f ./00-volume-config.yaml \
-                  ./00-volume-log.yaml \
-                  ./00-volume-storage.yaml \
-                  ./01-postgres-volume-storage.yaml \
-                  ./02-postgres-deployment.yaml \
-                  ./02-redis-deployment.yaml \
-                  ./03-deployment.yaml;
+              -f ./00-volume-log.yaml \
+              -f ./00-volume-storage.yaml \
+              -f ./01-postgres-volume-storage.yaml \
+              -f ./02-postgres-deployment.yaml \
+              -f ./02-redis-deployment.yaml \
+              -f ./03-deployment.yaml \
+              -f ./04-ingress.yaml;
