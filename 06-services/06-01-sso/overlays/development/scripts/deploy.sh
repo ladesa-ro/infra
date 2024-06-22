f896fa1594ddee56497568a8b098a7a79ec04bf3
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-22 18:19:21 -0400

    feat(services): rollout on deploy script

--- 06-services/06-01-sso/overlays/development/scripts/deploy.sh
+++ 06-services/06-01-sso/overlays/development/scripts/deploy.sh
@@ -4,4 +4,5 @@ set -xe
 
 if [[ "${LADESA_DEPLOY_SERVICE_SSO_DEVELOPMENT}" == "true" ]]; then
   kubectl apply -k ..;
+  kubectl rollout restart deployment.apps/ladesa-ro-sso --namespace ladesa-ro-development;
 fi
--- 06-services/06-01-sso/overlays/production/scripts/deploy.sh
+++ 06-services/06-01-sso/overlays/production/scripts/deploy.sh
@@ -4,4 +4,5 @@ set -xe
 
 if [[ "${LADESA_DEPLOY_SERVICE_SSO_PRODUCTION}" == "true" ]]; then
   kubectl apply -k ..;
+  kubectl rollout restart deployment.apps/ladesa-ro-sso --namespace ladesa-ro-production;
 fi
