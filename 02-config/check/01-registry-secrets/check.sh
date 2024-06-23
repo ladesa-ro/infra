7336020e8bfca3d3ee316b90b1347b689925c167
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-23 15:05:54 -0400

    fix(config/check/registry-secrets): `kubectl get secret` instead of `configmap`

--- 02-config/check/01-registry-secrets/check.sh
+++ 02-config/check/01-registry-secrets/check.sh
@@ -3,13 +3,13 @@
 set -xe
 
 if [[ "${LADESA_DEPLOY_NS_LOCAL}" == "true" && "${LADESA_DEPLOY_NS_LOCAL_REGISTRY}" == "true" ]]; then
-  kubectl get configmap ladesa-registry-pull-secret --namespace=ladesa-ro-local;
+  kubectl get secret ladesa-registry-pull-secret --namespace=ladesa-ro-local;
 fi
 
 if [[ "${LADESA_DEPLOY_NS_DEVELOPMENT}" == "true" && "${LADESA_DEPLOY_NS_DEVELOPMENT_REGISTRY}" == "true" ]]; then
-  kubectl get configmap ladesa-registry-pull-secret --namespace=ladesa-ro-development;
+  kubectl get secret ladesa-registry-pull-secret --namespace=ladesa-ro-development;
 fi
 
 if [[ "${LADESA_DEPLOY_NS_PRODUCTION}" == "true" && "${LADESA_DEPLOY_NS_PRODUCTION_REGISTRY}" == "true" ]]; then
-  kubectl get configmap ladesa-registry-pull-secret --namespace=ladesa-ro-production;
+  kubectl get secret ladesa-registry-pull-secret --namespace=ladesa-ro-production;
 fi
