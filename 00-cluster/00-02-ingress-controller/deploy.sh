96fe80df5ab26459a2dc1ee4ca38c064ac6cd472
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-23 17:36:51 -0400

    feat(cluster): setup ingress-nginx and cert-manager only if namespace does not exists

--- 00-cluster/00-02-ingress-controller/deploy.sh
+++ 00-cluster/00-02-ingress-controller/deploy.sh
@@ -10,4 +10,7 @@ if [[ ! "${LADESA_DEPLOY_SETUP_CLUSTER_INGRESS_CONTROLLER}" == "true" ]]; then
   exit 0;
 fi
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml;
+if [[ ! "$(kubectl get ns ingress-nginx &>/dev/null; echo $?)" == "0" ]]; then
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml;
+fi;
+
--- 00-cluster/00-04-cert-manager/deploy.sh
+++ 00-cluster/00-04-cert-manager/deploy.sh
@@ -10,6 +10,8 @@ if [[ ! "${LADESA_DEPLOY_SETUP_CLUSTER_CERT_MANAGER}" == "true" ]]; then
   exit 0;
 fi
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.yaml;
+if [[ ! "$(kubectl get ns cert-manager &>/dev/null; echo $?)" == "0" ]]; then
+  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.yaml;
+fi;
 
 cmctl check api --wait=2m;
