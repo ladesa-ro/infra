3d200ecec4800f5fcf7c8e3ba37e3db913d7cfc4
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-23 14:22:23 -0400

    feat(services): `ladesa-registry-pull-secret`

---
+++ 02-config/check/01-registry-secrets/check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+if [[ "${LADESA_DEPLOY_NS_LOCAL}" == "true" && "${LADESA_DEPLOY_NS_LOCAL_REGISTRY}" == "true" ]]; then
+  kubectl get configmap ladesa-registry-pull-secret --namespace=ladesa-ro-local;
+fi
+
+if [[ "${LADESA_DEPLOY_NS_DEVELOPMENT}" == "true" && "${LADESA_DEPLOY_NS_DEVELOPMENT_REGISTRY}" == "true" ]]; then
+  kubectl get configmap ladesa-registry-pull-secret --namespace=ladesa-ro-development;
+fi
+
+if [[ "${LADESA_DEPLOY_NS_PRODUCTION}" == "true" && "${LADESA_DEPLOY_NS_PRODUCTION_REGISTRY}" == "true" ]]; then
+  kubectl get configmap ladesa-registry-pull-secret --namespace=ladesa-ro-production;
+fi
--- 02-config/check/check.sh
+++ 02-config/check/check.sh
@@ -3,6 +3,7 @@
 set -xe
 
 (cd ./00-certificates; ./check.sh);
+(cd ./01-registry-secrets; ./check.sh);
 (cd ./config-registry; ./check.sh);
 (cd ./config-service-api; ./check.sh);
 (cd ./config-service-api-db; ./check.sh);
