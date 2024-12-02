7299a472810e98fca916340b6507dd8dc0a8e685
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-12-02 13:54:06 -0400

    Create registry-unreferenced-blobs.sh

---
+++ utils/cleanup/registry/registry-unreferenced-blobs.sh
@@ -0,0 +1,17 @@
+echo  ================================================================
+echo "Cleaning unreferenced registry blobs (registry garbage-collect)"
+echo  ================================================================
+
+for NAMESPACE in ${REGISTRY_NAMESPACES}
+do
+  REGISTRY_POD=$(kubectl get pods --selector "app=registry" --output=name -n ${NAMESPACE}; exit 0;);
+
+  for REGISTRY_POD in $REGISTRY_POD
+  do
+    if ! [ -z "$REGISTRY_POD" ]; then
+      (kubectl exec --tty --stdin ${REGISTRY_POD} -n ${NAMESPACE} -- registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true; exit 0);
+    fi;
+  done;
+done;
+
+echo "Unreferenced registry blobs garbage collected."
