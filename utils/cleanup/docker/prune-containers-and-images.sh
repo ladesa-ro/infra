ff91fdd2c280ef6fe3d8cebcfec05bc130892792
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-12-02 13:55:19 -0400

    Create prune-containers-and-images.sh

---
+++ utils/cleanup/docker/prune-containers-and-images.sh
@@ -0,0 +1,8 @@
+echo  ================================================================
+echo "Cleaning orphaned docker containers and images..."
+echo  ================================================================
+
+docker container prune -f;
+docker image prune -fa;
+
+echo "Orphaned docker containers and images collected."
