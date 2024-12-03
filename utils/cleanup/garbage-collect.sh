8a442a98c1a810ce94c132de999ce889e2164c06
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-12-02 23:49:01 -0400

    Update garbage-collect.sh

--- utils/cleanup/garbage-collect.sh
+++ utils/cleanup/garbage-collect.sh
@@ -7,9 +7,9 @@ echo "Starting cleanup..."
 export PROJECT_NAMESPACES="ladesa-ro-production ladesa-ro-development ladesa-ro-local"
 export REGISTRY_NAMESPACES=${PROJECT_NAMESPACES}
 
-./cluster/k8s-orphaned-replicasets.sh;
-./registry/registry-unreferenced-blobs.sh;
-./docker/prune-containers-and-images.sh;
-./cluster/kind/nodes-images-cache-prune.sh;
+bash ./cluster/k8s-orphaned-replicasets.sh;
+bash ./registry/registry-unreferenced-blobs.sh;
+bash ./docker/prune-containers-and-images.sh;
+bash ./cluster/kind/nodes-images-cache-prune.sh;
 
 echo "Cleanup Done! Thanks."
