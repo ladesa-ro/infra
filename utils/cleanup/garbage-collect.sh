8e0e61c1c9112497f27fc34e72f80f71f813f0e9
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2024-12-02 23:39:35 -0400

    Update garbage-collect.sh

--- utils/cleanup/garbage-collect.sh
+++ utils/cleanup/garbage-collect.sh
@@ -4,62 +4,12 @@ set -e;
 
 echo "Starting cleanup..."
 
-PROJECT_NAMESPACES="ladesa-ro-production ladesa-ro-development ladesa-ro-local"
-REGISTRY_NAMESPACES=${PROJECT_NAMESPACES}
+export PROJECT_NAMESPACES="ladesa-ro-production ladesa-ro-development ladesa-ro-local"
+export REGISTRY_NAMESPACES=${PROJECT_NAMESPACES}
 
-echo  ================================================================
-echo "Cleaning orphaned replicasets"
-echo  ================================================================
-
-
-for NAMESPACE in ${PROJECT_NAMESPACES}
-do
-  REPLICAS=$(kubectl get replicaset -n ${NAMESPACE} -o jsonpath='{ .items[?(@.spec.replicas==0)].metadata.name }'; exit 0);
-
-  if ! [ -z "$REPLICAS" ]; then
-    ((kubectl delete replicaset -n ${NAMESPACE} ${REPLICAS}); exit 0;);
-  fi;
-
-done;
-
-echo "Orphaned replicasets collected."
-
-echo  ================================================================
-echo "Cleaning unreferenced registry blobs (registry garbage-collect)"
-echo  ================================================================
-
-for NAMESPACE in ${REGISTRY_NAMESPACES}
-do
-  REGISTRY_POD=$(kubectl get pods --selector "app=registry" --output=name -n ${NAMESPACE}; exit 0;);
-
-  for REGISTRY_POD in $REGISTRY_POD
-  do
-    if ! [ -z "$REGISTRY_POD" ]; then
-      (kubectl exec --tty --stdin ${REGISTRY_POD} -n ${NAMESPACE} -- registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true; exit 0);
-    fi;
-  done;
-done;
-
-echo "Unreferenced registry blobs garbage collected."
-
-echo  ================================================================
-echo "Cleaning orphaned docker containers and images..."
-echo  ================================================================
-
-docker container prune -f;
-docker image prune -fa;
-
-echo "Orphaned docker containers and images collected."
-
-echo  ================================================================
-echo "Cleaning orphaned images inside kind cluster nodes..."
-echo  ================================================================
-
-for nodeName in $(kind get nodes)
-do
-  docker exec "${nodeName}" bash -c "crictl rmi --prune";
-done;
-
-echo  ================================================================
+./cluster/k8s-orphaned-replicasets.sh;
+./registry/registry-unreferenced-blobs.sh;
+./docker/prune-containers-and-images.sh;
+./cluster/kind/nodes-images-cache-prune.sh;
 
 echo "Cleanup Done! Thanks."
